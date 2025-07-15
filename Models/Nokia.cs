09d07463a1d010067ecfe41b3d626100de6fcf26
Rafael Miranda <rafaelmiranda@academico.ufs.br>
2025-07-15 02:30:14 -0300

    Desafio Concluido

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,19 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia:Smartphone
     {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp){
+            Console.WriteLine($"Aplicativo {nomeApp} instalado no Nokia {Modelo}...");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("7998450000", "xlr8","1sc65",64);
+Nokia nokia= new Nokia("7988523301","tijolao","phonk13",32);
+nokia.InstalarAplicativo("Fruit Ninja");
+iphone.InstalarAplicativo("Facetune");
+nokia.Ligar();
+iphone.ReceberLigacao();
